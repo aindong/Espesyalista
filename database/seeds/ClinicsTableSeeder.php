75e5b0ae66dadd1186679fbca999da01080df26b
Alleo Indong <freedom_war550@live.com.ph>
2015-08-09 00:40:14 +0800

    Clinics seeder

---
+++ database/seeds/ClinicsTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClinicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Clinic::create([
+            'name'      => 'Philcare Makati',
+            'location'  => serialize([
+                'lat' => 14.55609,
+                'lon' => 121.02189
+            ])
+        ]);
+
+        \App\Models\Clinic::create([
+            'name'      => 'Aventus Medical Care',
+            'location'  => serialize([
+                'lat' => 14.5702459,
+                'lon' => 121.0217766
+            ])
+        ]);
+
+        \App\Models\Clinic::create([
+            'name'      => 'FortMED Medical Clinic',
+            'location'  => serialize([
+                'lat' => 14.5630284,
+                'lon' => 121.0224857
+            ])
+        ]);
+
+        \App\Models\Clinic::create([
+            'name'      => 'Biomedix Inc',
+            'location'  => serialize([
+                'lat' => 14.558418,
+                'lon' => 121.014383
+            ])
+        ]);
+
+        \App\Models\Clinic::create([
+            'name'      => 'HP Plus',
+            'location'  => serialize([
+                'lat' => 14.4898456,
+                'lon' => 121.0397747
+            ])
+        ]);
+    }
+}

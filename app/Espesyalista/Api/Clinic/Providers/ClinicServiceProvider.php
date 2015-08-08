215502feacbf0147e6cc38649e8abbe7b66e2424
Alleo Indong <freedom_war550@live.com.ph>
2015-08-08 22:33:31 +0800

    Created Clinics Crud

--- app/Espesyalista/Api/Clinic/Providers/ClinicServiceProvider.php
+++ app/Espesyalista/Api/Clinic/Providers/ClinicServiceProvider.php
@@ -2,10 +2,9 @@
 namespace App\Espesyalista\Api\Clinic\Providers;
 
 use App;
-use Config;
+use Illuminate\Support\ServiceProvider;
 use Lang;
 use View;
-use Illuminate\Support\ServiceProvider;
 
 class ClinicServiceProvider extends ServiceProvider
 {
@@ -35,4 +34,14 @@ class ClinicServiceProvider extends ServiceProvider
 
 		View::addNamespace('clinic', realpath(__DIR__.'/../Resources/Views'));
 	}
+
+    /**
+     * Bind the patientrepointerface to patientrepo
+     */
+    protected function bindClinicRepositories()
+    {
+        $this->app->bind('\App\Espesyalista\Api\Clinic\Interfaces\ClinicsRepoInterface', function ($app) {
+            return new App\Espesyalista\Api\Clinic\Repositories\ClinicsRepo(new App\Models\User);
+        });
+    }
 }

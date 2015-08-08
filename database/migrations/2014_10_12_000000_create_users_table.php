ca827c2d45f2090f476b465690cd3e710021463a
Alleo Indong <freedom_war550@live.com.ph>
2015-08-08 22:46:22 +0800

    Created schema for clinic services

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateUsersTable extends Migration
 {
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('group_id')->unsinged();
+            $table->integer('clinic_id')->unsinged();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('middlename');

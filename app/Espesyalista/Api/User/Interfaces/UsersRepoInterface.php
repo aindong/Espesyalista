a7bf0dcbc8317778465ee9250d5e8282dcfcebbf
Alleo Indong <freedom_war550@live.com.ph>
2015-08-08 17:38:05 +0800

    Users repo and users repo interface

---
+++ app/Espesyalista/Api/User/Interfaces/UsersRepoInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Espesyalista\User\Interfaces;
+
+use App\Espesyalista\Base\Interfaces\EloquentInterface;
+
+interface UsersRepoInterface extends EloquentInterface
+{
+
+}
---
+++ app/Espesyalista/Api/User/Repositories/UsersRepo.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Espesyalista\User\Repositories;
+
+use App\Espesyalista\Base\Repositories\EloquentRepository;
+use App\Espesyalista\User\Interfaces\UsersRepoInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersRepo extends EloquentRepository implements UsersRepoInterface
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+}

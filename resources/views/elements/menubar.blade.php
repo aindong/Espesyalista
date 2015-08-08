463933f9f253454e7a1ce0bb3514addf8aa68855
Alleo Indong <freedom_war550@live.com.ph>
2015-08-08 15:22:57 +0800

    Added design files

---
+++ resources/views/elements/menubar.blade.php
@@ -0,0 +1,126 @@
+<div id="menubar" class="menubar-inverse ">
+    <div class="menubar-fixed-panel">
+        <div>
+            <a class="btn btn-icon-toggle btn-default menubar-toggle" data-toggle="menubar" href="javascript:void(0);">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        <div class="expanded">
+            <a href="/">
+                <span class="text-lg text-bold text-primary ">OBClinic</span>
+            </a>
+        </div>
+    </div>
+    <div class="menubar-scroll-panel">
+        <!-- BEGIN MAIN MENU -->
+
+
+
+
+        <ul id="main-menu" class="gui-controls">
+            <!-- BEGIN DASHBOARD -->
+            <li>
+                <a href="/#" class="">
+                    <div class="gui-icon"><i class="md md-home"></i></div>
+                    <span class="title">Dashboard</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END DASHBOARD -->
+
+            <!-- BEGIN Patients -->
+            <li class="gui-folder">
+                <a>
+                    <div class="gui-icon"><i class="md md-view-agenda"></i></div>
+                    <span class="title">Maintenance</span>
+                </a>
+                <!--start submenu -->
+                <ul>
+                    <li><a href="/#maintenance/users" ><span class="title">Users</span></a></li> <!-- TODO -->
+                    <li><a href="/#maintenance/allergies" ><span class="title">Allergies</span></a></li>
+                    <li><a href="/#maintenance/diseases" ><span class="title">Diseases</span></a></li>
+                    <li><a href="/#maintenance/medicines" ><span class="title">Medicines</span></a></li>
+                    <li><a href="/#maintenance/procedures" ><span class="title">Procedures</span></a></li> <!-- TODO -->
+                    <li><a href="/#maintenance/diagnosis" ><span class="title">Diagnosis</span></a></li> <!-- TODO -->
+                    <li><a href="/#maintenance/complaints" ><span class="title">Complaints</span></a></li> <!-- TODO -->
+
+                </ul><!--end /submenu -->
+            </li><!--end /menu-li -->
+            <!-- END Patients -->
+
+            <!-- BEGIN Appointments -->
+            <li>
+                <a href="/#appointments">
+                    <div class="gui-icon"><i class="md md-schedule"></i></div>
+                    <span class="title">Appointments</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END Appointments -->
+
+            <!-- BEGIN Patients -->
+            <li class="gui-folder">
+                <a>
+                    <div class="gui-icon"><i class="md md-group"></i></div>
+                    <span class="title">Patients</span>
+                </a>
+                <!--start submenu -->
+                <ul>
+                    <li><a href="/#patients" ><span class="title">List</span></a></li>
+                </ul><!--end /submenu -->
+            </li><!--end /menu-li -->
+            <!-- END Patients -->
+
+            <!-- BEGIN Billing -->
+            <li>
+                <a href="">
+                    <div class="gui-icon"><i class="md md-payment"></i></div>
+                    <span class="title">Billing</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END Billing -->
+
+            <!-- BEGIN Inventory -->
+            <li>
+                <a href="">
+                    <div class="gui-icon"><i class="md md-receipt"></i></div>
+                    <span class="title">Inventory</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END Inventory -->
+
+            <!-- BEGIN Reports -->
+            <li>
+                <a href="">
+                    <div class="gui-icon"><i class="md md-trending-up"></i></div>
+                    <span class="title">Reports</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END Reports -->
+
+            <!-- BEGIN Miscellaneous -->
+            <li>
+                <a href="">
+                    <div class="gui-icon"><i class="md md-language"></i></div>
+                    <span class="title">Miscellaneous</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END Miscellaneous -->
+
+            <!-- BEGIN Settings -->
+            <li>
+                <a href="">
+                    <div class="gui-icon"><i class="md md-settings"></i></div>
+                    <span class="title">Settings</span>
+                </a>
+            </li><!--end /menu-li -->
+            <!-- END Settings -->
+
+        </ul><!--end .main-menu -->
+        <!-- END MAIN MENU -->
+
+        <div class="menubar-foot-panel">
+            <small class="no-linebreak hidden-folded">
+                <span class="opacity-75">Copyright &copy; 2015</span> <strong>Aindong</strong>
+            </small>
+        </div>
+    </div><!--end .menubar-scroll-panel-->
+</div><!--end #menubar-->

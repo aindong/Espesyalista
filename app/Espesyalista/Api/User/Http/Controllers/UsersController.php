6700c7f9c2448a94a5e0d750fdf91314f298ab63
Alleo Indong <freedom_war550@live.com.ph>
2015-08-08 17:27:59 +0800

    Updated users controller

---
+++ app/Espesyalista/Api/User/Http/Controllers/UsersController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Espesyalista\User;
+
+use App\Espesyalista\Base\Controllers\ApiController;
+
+class UsersController extends ApiController
+{
+    public function __construct()
+    {
+
+    }
+}

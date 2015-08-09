e8ea2db5389a395c5e86f9953806f8104b0cb0d7
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-08-09 10:04:21 +0800

    booking

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,7 +38,7 @@ Route::get('user/login', function() {
 });
 
 Route::get('user/registration', function() {
-    return view('users.login');
+    return view('users.registration');
 });
 
 // Appointments

8e42ce9276cb55545afb3b8b6062fa35719f6925
Wayne <markwaynepulido24@gmail.com>
2015-08-09 10:39:59 +0800

    Edit login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -2,29 +2,39 @@
 
 @section('content')
     <style>
-        .container{
+        .container {
             margin-top:100px;
+            text-align:center;
+        }
+        .align_center {
+            text-align: center;
         }
     </style>
 
     <div class="container">
         <form action="">
-            <div class="form-group">
-                <label>FirstName</label>
-                <input type="text" class="form-control">
-            </div>
+            <form class="form-sign">
+                <h2 class="form-signin-heading">Sign in</h2>
+                <label for="inputFirstname" class="sr-only">Firstname</label>
+                <input type="text" id="inputFirstname" class="form-control" placeholder="Firstname" required autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
 
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control">
-            </div>
-            <botton type="button" class="btn btn-info"></botton>
+                <div class="checkbox">
+                    <label><input type="checkbox" value="remember-me">"Remember me"</label>
+                    <button class="btn btn--lg btn-danger btn-block" type="submit">Sign in</button>
+                            <div class="align_center">
+                                <h5>or sign up using</h5>
+                                <div class="social-icons">
+                                    <a href="/login/facebook"><i class="fa fa-facebook-square fa-3x" style="color:#6080c5 ;"></i></a>
+                                    <a href="/login/github"><i class="fa fa-github-square fa-3x" style="color:#716868 ;"></i></a>
+                                    <a href="/login/twitter"><i class="fa fa-twitter-square fa-3x" style="color:#1dcaff ;"></i></a>
+                                    <a href="/login/google"><i class="fa fa-google-plus-square fa-3x" style="color:#d6412d ;"></i></a>
+                                </div>
+                            </div>
 
-            <div class="container">
-                <div class="row">
-                    <a href="" class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-thumbs-up"></span></a>
-                </div>
-            </div>
+            </form>
+
         </form>
 
     </div>
